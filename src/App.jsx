7ed7aaf415fce6eccde5ac30af72b27186963f37
tank <exppro1@gmail.com>
2020-06-27 14:22:41 +0300

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +12,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
